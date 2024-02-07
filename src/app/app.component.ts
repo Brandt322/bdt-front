02191859d8020d7d52a878a85c4f69046d602534
Brandt322 <bgamboaa98@gmail.com>
2024-02-07 11:33:40 -0500

    feat: :rocket: created the initial project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'bdt';
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    initFlowbite();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core/core.module';
+import { FeaturesModule } from './features/features.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +15,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    SharedModule,
+    CoreModule,
+    FeaturesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
