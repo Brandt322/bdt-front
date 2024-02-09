fa0334e8de859a49f2ad3c8cb31159441633621c
Oliver Castillo <olcastillot@gmail.com>
2024-02-08 19:31:33 -0500

    feat: add stars rating

--- src/app/features/talent/talent-create/talent-create.component.ts
+++ src/app/features/talent/talent-create/talent-create.component.ts
@@ -1,11 +1,37 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-talent-create',
   templateUrl: './talent-create.component.html',
   styleUrls: ['./talent-create.component.css'],
 })
-export class TalentCreateComponent {}
+export class TalentCreateComponent implements OnInit {
+  @Input() maxRatting: number = 5;
+  @Input() SelectedStar: number = 0;
+  maxRattingArr: any = [];
+  previousSelection: number = 0;
+  constructor() {}
+
+  ngOnInit(): void {
+    this.maxRattingArr = Array(this.maxRatting).fill(0);
+  }
+
+  HandleMouseEnter(index: number) {
+    this.SelectedStar = index + 1;
+  }
+
+  HandleMouseLeave() {
+    if (this.previousSelection !== 0) {
+      this.SelectedStar = this.previousSelection;
+    } else {
+      this.SelectedStar = 0;
+    }
+  }
+  HandleClickRating(index: number) {
+    this.SelectedStar = index + 1;
+    this.previousSelection = this.SelectedStar;
+  }
+}
 
 /* Checkbox */
 @Component({
