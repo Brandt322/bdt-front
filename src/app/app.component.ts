63ecadc591748a2320bf201b4907c0992d108ffe
Brandt322 <bgamboaa98@gmail.com>
2024-02-11 14:06:30 -0500

    feat: :sparkles: implement a global loader component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { initFlowbite } from 'flowbite';
+import { LoaderService } from './core/global/loader/loader.service';
 
 @Component({
   selector: 'app-root',
@@ -7,7 +8,15 @@ import { initFlowbite } from 'flowbite';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+
+  constructor(public loader: LoaderService) { }
+
   ngOnInit(): void {
     initFlowbite();
+
+    this.loader.showLoader();
+    setTimeout(() => {
+      this.loader.hideLoader();
+    }, 3000)
   }
 }
