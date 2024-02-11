60f66e28612c268e9b4e0ddd7e5a77dacb7cea65
Oliver Castillo <olcastillot@gmail.com>
2024-02-11 18:53:52 -0500

    fix: modularize inputs components

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,17 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from '../app-routing.module';
 import { HeaderComponent } from './components/layout/header/header.component';
 import { MainComponent } from './components/layout/main/main.component';
-import { AppRoutingModule } from '../app-routing.module';
-import { RadioModalComponent } from './components/utils/modals/radio-modal.component';
-import { CheckboxModalComponent } from './components/utils/modals/checkbox-modal.component';
 import { ButtonDropdownComponent } from './components/utils/buttons/button-dropdown.component';
 import { ProfileComponent } from './components/utils/cards/profile/profile.component';
+import { AddSkillButtonComponent } from './components/utils/form-inputs/add-skill-component';
+import { CheckboxInputComponent } from './components/utils/form-inputs/checkbox-input-component';
+import { FileInputComponent } from './components/utils/form-inputs/file-input-component';
+import { SubtitleFormComponent } from './components/utils/form-inputs/subtitle-form-component';
+import { TextInputComponent } from './components/utils/form-inputs/text-input-component';
+import { CheckboxModalComponent } from './components/utils/modals/checkbox-modal.component';
+import { RadioModalComponent } from './components/utils/modals/radio-modal.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +20,13 @@ import { ProfileComponent } from './components/utils/cards/profile/profile.compo
     RadioModalComponent,
     CheckboxModalComponent,
     ButtonDropdownComponent,
-    ProfileComponent
+    ProfileComponent,
+    /* Input */
+    CheckboxInputComponent,
+    TextInputComponent,
+    SubtitleFormComponent,
+    FileInputComponent,
+    AddSkillButtonComponent,
   ],
   imports: [CommonModule, AppRoutingModule],
   exports: [
@@ -24,7 +35,13 @@ import { ProfileComponent } from './components/utils/cards/profile/profile.compo
     RadioModalComponent,
     CheckboxModalComponent,
     ButtonDropdownComponent,
-    ProfileComponent
+    ProfileComponent,
+    /* Form Inputs */
+    CheckboxInputComponent,
+    TextInputComponent,
+    SubtitleFormComponent,
+    FileInputComponent,
+    AddSkillButtonComponent,
   ],
 })
-export class SharedModule { }
+export class SharedModule {}
