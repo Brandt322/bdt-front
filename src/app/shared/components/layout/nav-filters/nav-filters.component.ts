7e81a0ecf5dee5e4013c2ca5b2c8cd06f2ac0639
Brandt322 <bgamboaa98@gmail.com>
2024-02-15 11:58:25 -0500

    feat: :sparkles: separating dashboard content into components

---
+++ src/app/shared/components/layout/nav-filters/nav-filters.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MasterService } from 'src/app/services/master/master.service';
+import { Level } from 'src/app/shared/models/interfaces/level-interface';
+import { API_ENDPOINTS } from 'src/app/core/global/constants/api-endpoints';
+
+@Component({
+  selector: 'app-nav-filters',
+  templateUrl: './nav-filters.component.html',
+  styleUrls: ['./nav-filters.component.css']
+})
+export class NavFiltersComponent implements OnInit {
+  data: Level[] = [];
+
+  constructor(private router: Router, private masterService: MasterService) { }
+
+  ngOnInit(): void {
+    this.masterService.getLevel(API_ENDPOINTS.NIVELES).subscribe(niveles => {
+      this.data = niveles
+    });
+  }
+
+  onButtonClick() {
+    this.router.navigate(['/main/new-talent']);
+  }
+}
---
+++ src/app/shared/components/layout/talent-content/talent-content.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-talent-content',
+  templateUrl: './talent-content.component.html',
+  styleUrls: ['./talent-content.component.css']
+})
+export class TalentContentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
