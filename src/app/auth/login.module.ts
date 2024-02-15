1bf83d908a618bb728556632829e292c64ed92c5
Oliver Castillo <olcastillot@gmail.com>
2024-02-15 12:49:32 -0500

    feat: update login design

--- src/app/auth/login.module.ts
+++ src/app/auth/login.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { SharedModule } from '../shared/shared.module';
 import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [LoginComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, SharedModule],
   exports: [LoginComponent],
 })
 export class LoginModule {}
